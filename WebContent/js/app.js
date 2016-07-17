0efa66854c9147154ae155ecc82ddfebb1a3df4a
AnushaSekhar <anusha141991@gmail.com>
2016-07-17 21:48:15 +0530

    Add files via upload

---
+++ WebContent/js/app.js
@@ -0,0 +1,17 @@
+﻿
+
+// angular.js main app initialization
+var app = angular.module('website', ['ngRoute']);
+app.controller('mainCtrl',function(){
+	alert("xcbvjhf");
+});
+ app.config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+        when('/main', { 
+        	templateUrl: 'pages/main.html',  
+        	controller: 'mainCtrl'
+        		})
+    }]);
+
+
+
---
+++ WebContent/js/controllers.js
@@ -0,0 +1,12 @@
+﻿
+
+// optional controllers
+
+function ProjectCtrl($scope, $http) {
+}
+
+function PrivacyCtrl($scope, $http, $timeout) {
+}
+
+function AboutCtrl($scope, $http, $timeout) {
+}
